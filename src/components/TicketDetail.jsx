77c06008489db669d79819c0fb7b1a24ab3d459d
bigsaigon333 <likepeppermint@gmail.com>
2021-04-17 21:50:56 +0900

    fix: 로또 티켓 정보 확인 토글 버튼의 제어 추가

--- src/components/TicketDetail.jsx
+++ src/components/TicketDetail.jsx
@@ -18,7 +18,7 @@ export default class TicketDetail extends React.Component {
 
   handleChange(event) {
     this.setState({
-      isDetail: event.target.checked,
+      isDetail: this.props.tickets.length > 0 && event.target.checked,
     });
   }
 
@@ -26,25 +26,25 @@ export default class TicketDetail extends React.Component {
     return (
       <section className="mt-6">
         <div className="flex justify-between mb-2">
-          <h2 className="text-xl font-semibold">총 {this.props.tickets.length}개를 구매하였습니다.</h2>
+          <h2 className="text-xl font-semibold select-none">총 {this.props.tickets.length}개를 구매하였습니다.</h2>
           <div className="flex items-center justify-center ">
             <label className="flex items-center cursor-pointer">
               <div className="relative">
-                <input type="checkbox" className="sr-only" onChange={this.handleChange} />
+                <input type="checkbox" className="sr-only" onChange={this.handleChange} checked={this.state.isDetail} />
                 <div className="w-10 h-4 bg-gray-400 rounded-full shadow-inner" />
                 <div className="dot absolute w-6 h-6 bg-white rounded-full shadow -left-1 -top-1 checked:transform checked:translate-x-full checked:bg-red-100" />
               </div>
-              <div className="ml-3 text-gray-700 font-medium">번호 보기</div>
+              <div className="ml-3 text-gray-700 font-medium select-none">번호 보기</div>
             </label>
           </div>
         </div>
-        <div className={`flex flex-wrap ${this.state.isDetail && 'flex-col'} detail-mode`}>
+        <div className={`flex flex-wrap ${this.state.isDetail ? 'flex-col' : 'flex-row'}`}>
           {this.props.tickets.map((ticket, index) => (
             <div key={ticket.join(index)} className="flex items-center my-2">
-              <span className="mx-1 text-4xl mr-2" role="img" aria-label="lotto-ticket">
+              <span className="mx-1 text-4xl mr-2 select-none" role="img" aria-label="lotto-ticket">
                 🎟️
               </span>
-              {this.state.isDetail && <span className="text-lg">{ticket.join(', ')}</span>}
+              {this.state.isDetail && <span className="text-lg select-all">{ticket.join(', ')}</span>}
             </div>
           ))}
         </div>
